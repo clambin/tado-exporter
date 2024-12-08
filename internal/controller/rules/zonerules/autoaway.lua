638cf6fc93b1b2a96a1424eddb8f6d34f7fb182e
Christophe Lambin <christophe.lambin@gmail.com>
2024-12-08 19:59:37 +0100

    reformat lua files

--- internal/controller/rules/zonerules/autoaway.lua
+++ internal/controller/rules/zonerules/autoaway.lua
@@ -1,19 +1,19 @@
 function Evaluate(_, state, devices, _)
-        if #devices == 0 then
-            return state, 0, "no devices found"
-        end
-        local homeUsers = FilterDevices(devices, true)
-        local wantHeating = #homeUsers > 0
-        local reason = "all users are away: " .. ListDevices(devices)
-        local delay = 900
-        if wantHeating then
-            reason = "one or more users are home: " .. ListDevices(homeUsers)
-            delay = 0
-        end
-        if state.Heating == wantHeating then
-            return state, 0, reason
-        end
-        return { Overlay = not wantHeating, Heating = wantHeating }, delay, reason
+    if #devices == 0 then
+        return state, 0, "no devices found"
+    end
+    local homeUsers = FilterDevices(devices, true)
+    local wantHeating = #homeUsers > 0
+    local reason = "all users are away: " .. ListDevices(devices)
+    local delay = 900
+    if wantHeating then
+        reason = "one or more users are home: " .. ListDevices(homeUsers)
+        delay = 0
+    end
+    if state.Heating == wantHeating then
+        return state, 0, reason
+    end
+    return { Overlay = not wantHeating, Heating = wantHeating }, delay, reason
 end
 
 function FilterDevices(devices, state)
@@ -27,9 +27,9 @@ function FilterDevices(devices, state)
 end
 
 function ListDevices(devices)
-	local result = ""
-	for _, obj in ipairs(devices) do
-		result = result .. obj.Name .. ", "
-	end
-	return result:sub(1, -3)
+    local result = ""
+    for _, obj in ipairs(devices) do
+        result = result .. obj.Name .. ", "
+    end
+    return result:sub(1, -3)
 end
--- internal/controller/rules/zonerules/limitoverlay.lua
+++ internal/controller/rules/zonerules/limitoverlay.lua
@@ -2,9 +2,9 @@ function Evaluate(_, zone, _, _)
     if not zone.Heating then
         return zone, 0, "heating is off"
     end
-	if zone.Overlay then
-    	zone.Overlay = false
-	    return zone, 3600, "manual setting detected"
-	end
+    if zone.Overlay then
+        zone.Overlay = false
+        return zone, 3600, "manual setting detected"
+    end
     return zone, 0, "no manual setting detected"
 end
