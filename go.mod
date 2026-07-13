b006c980494512d2c4d2686c2f6b5d8fafec53a6
Christophe Lambin <christophe.lambin@gmail.com>
2026-07-13 22:53:16 +0200

    chore(deps): bump codeberg.org/clambin/proteus to v0.8.0

--- go.mod
+++ go.mod
@@ -1,29 +1,28 @@
 module github.com/clambin/tado-exporter
 
-go 1.25
+go 1.26
 
 require (
 	codeberg.org/clambin/go-common/flagger v0.3.0
-	codeberg.org/clambin/proteus v0.6.0
-	github.com/clambin/tado/v2 v2.6.2
+	codeberg.org/clambin/proteus v0.8.0
+	github.com/clambin/tado/v2 v2.7.0
 	github.com/prometheus/client_golang v1.23.2
-	golang.org/x/oauth2 v0.34.0
+	golang.org/x/oauth2 v0.36.0
 )
 
 require (
-	codeberg.org/clambin/go-common/set v0.6.0 // indirect
+	codeberg.org/clambin/go-crypt v0.1.1 // indirect
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/oapi-codegen/runtime v1.1.1 // indirect
+	github.com/oapi-codegen/runtime v1.4.2 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
-	github.com/prometheus/common v0.67.4 // indirect
+	github.com/prometheus/common v0.67.5 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
-	github.com/rogpeppe/go-internal v1.14.1 // indirect
-	go.yaml.in/yaml/v2 v2.4.3 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	google.golang.org/protobuf v1.36.10 // indirect
+	go.yaml.in/yaml/v2 v2.4.4 // indirect
+	golang.org/x/crypto v0.53.0 // indirect
+	golang.org/x/sys v0.46.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
--- tado.go
+++ tado.go
@@ -9,8 +9,9 @@ import (
 	"syscall"
 
 	"codeberg.org/clambin/go-common/flagger"
-	"codeberg.org/clambin/proteus/tado/collector"
-	"github.com/clambin/tado/v2"
+	"codeberg.org/clambin/proteus/integrations/climate"
+	"codeberg.org/clambin/proteus/integrations/climate/tado"
+	tado2 "github.com/clambin/tado/v2"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/oauth2"
 )
@@ -40,11 +41,11 @@ func main() {
 	flag.Parse()
 	logger := cfg.Logger(os.Stderr, nil)
 	if cfg.Token.Path == "" {
-		logger.Error("token path is required")
+		logger.Error("token storage path is required")
 		os.Exit(1)
 	}
 	if cfg.Passphrase == "" {
-		logger.Error("token passphrase is required")
+		logger.Error("token storage passphrase is required")
 		os.Exit(1)
 	}
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
@@ -58,9 +59,15 @@ func main() {
 
 	logger.Debug("created Tado client", "tokenPath", cfg.Token.Path)
 
-	poller := collector.Poller{Client: client, Logger: logger}
-	coll := collector.NewCollector(nil, &poller, "", logger)
-	prometheus.MustRegister(coll)
+	c := collector{
+		scraper: &tado.Scraper{
+			Client:      client,
+			Logger:      logger,
+			Descriptors: climate.NewMetricDescriptors("tado"),
+		},
+	}
+
+	prometheus.MustRegister(c)
 
 	logger.Info("tado exporter starting", "version", version)
 
@@ -73,14 +80,33 @@ func makeTadoClient(
 	ctx context.Context,
 	path string,
 	passphrase string,
-) (*tado.ClientWithResponses, error) {
+) (*tado2.ClientWithResponses, error) {
 	// create an oauth2 http client that uses the device auth flow
-	httpClient, err := tado.NewOAuth2Client(ctx, path, passphrase, func(response *oauth2.DeviceAuthResponse) {
+	httpClient, err := tado2.NewOAuth2Client(ctx, path, passphrase, func(response *oauth2.DeviceAuthResponse) {
 		fmt.Printf("No token found. Visit %s and log in ...\n", response.VerificationURIComplete)
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to create oauth2 client: %w", err)
 	}
 	// create the tado client using the oauth2 http client
-	return tado.NewClientWithResponses(tado.ServerURL, tado.WithHTTPClient(httpClient))
+	return tado2.NewClientWithResponses(tado2.ServerURL, tado2.WithHTTPClient(httpClient))
+}
+
+var _ prometheus.Collector = collector{}
+
+type collector struct {
+	scraper *tado.Scraper
+}
+
+func (c collector) Describe(ch chan<- *prometheus.Desc) {
+	c.scraper.Descriptors.Describe(ch)
+}
+
+func (c collector) Collect(ch chan<- prometheus.Metric) {
+	metrics, err := c.scraper.Scrape(context.Background())
+	if err != nil {
+		ch <- prometheus.NewInvalidMetric(prometheus.NewDesc("tado_scrape_error", "error scraping tado", nil, nil), err)
+		return
+	}
+	metrics.Collect(ch)
 }
